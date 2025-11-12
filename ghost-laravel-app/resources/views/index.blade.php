2479650ecc61931b246e4007e5942367079759c5
Polina Sterobo <ppbs@intellifinder.dk>
2025-11-12 12:23:06 +0100

    lesson 13 -- directives (if else in "frontend")

--- ghost-laravel-app/resources/views/index.blade.php
+++ ghost-laravel-app/resources/views/index.blade.php
@@ -1,8 +1,25 @@
-<h1>Hello</h1>
-<p>This is just a text</p>
+<h1>Welcome</h1>
+
 {{-- directive -- this is everything that starts with @,
  this prevents errors and executes code conditionally --}}
+{{-- @isset($name) <p>Welcome back: {{ $name }}</p>@endisset() --}}
 
-@isset($name)
-    <p>Welcome back: {{ $name }}</p>
-@endisset()
+<div>
+    {{-- @if (count($tasks))
+    //this dumps all tasks as a list the same as foreach
+    , but it have built in if else feature .  <div>Todays to-do's</div> @foreach ($tasks as $task)
+    <li>{{ $task->title }}</li>  @endforeach @else <p> There are no tasks</p> @endif --}}
+
+
+    <h3>Todays to-do's: </h3>
+
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+                <li>{{ $task->title }}</li>
+            </a>
+        </div>
+    @empty
+        <p>There are no tasks</p>
+    @endforelse
+</div>
--- ghost-laravel-app/routes/web.php
+++ ghost-laravel-app/routes/web.php
@@ -2,26 +2,83 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+class Task
+{
+    public function __construct(
+        public int $id,
+        public string $title,
+        public string $description,
+        public ?string $long_description,
+        public bool $completed,
+        public string $created_at,
+        public string $updated_at
+    ) {
+    }
+}
+
+$tasks = [
+  new Task(
+      1,
+      'Buy groceries',
+      'Task 1 description',
+      'Task 1 long description',
+      false,
+      '2023-03-01 12:00:00',
+      '2023-03-01 12:00:00'
+  ),
+  new Task(
+      2,
+      'Sell old stuff',
+      'Task 2 description',
+      null,
+      false,
+      '2023-03-02 12:00:00',
+      '2023-03-02 12:00:00'
+  ),
+  new Task(
+      3,
+      'Learn programming',
+      'Task 3 description',
+      'Task 3 long description',
+      true,
+      '2023-03-03 12:00:00',
+      '2023-03-03 12:00:00'
+  ),
+  new Task(
+      4,
+      'Take dogs for a walk',
+      'Task 4 description',
+      null,
+      false,
+      '2023-03-04 12:00:00',
+      '2023-03-04 12:00:00'
+  ),
+];
+
+//you need to utilize use() function since return here is anonymous, and data will not be available otherwise
+Route::get('/', function () use ($tasks) {
     //return 'Main Page'; //this is the lending page
-    return view(
-        'index',
-        ['name' => 'John Doe']
-    ); // you can manually just dump data here
-});
+    //return view('index', ['name' => 'John Doe']); // you can manually just dump data here
+    return view('index', [ 'tasks' => $tasks]);
 
-Route::get('/xxx', function () {
-    return 'Hello';
-})->name('route-name'); //when you name a route you give it alias that can always be used to identify it
+}) -> name('tasks.index');
 
-Route::get('/wrong', function () {
-    return redirect()->route('hello');
-}); //this helps to redirect different path somewhere
+Route::get('/{id}', function ($id) {
+    return 'this is is details of a task';
+}) -> name('tasks.show');
 
-Route::get('greet/{name}', function ($name) {
-    return 'Hello ' . $name . '!';
-}); //this accepts the value directly and shows it
+// Route::get('/xxx', function () {
+//     return 'Hello';
+// })->name('route-name'); //when you name a route you give it alias that can always be used to identify it
 
-Route::fallback(function () {
-    return 'This path does not exist!';
-}); // fallback redirect all wrong path and shows this message
+// Route::get('/wrong', function () {
+//     return redirect()->route('hello');
+// }); //this helps to redirect different path somewhere
+
+// Route::get('greet/{name}', function ($name) {
+//     return 'Hello ' . $name . '!';
+// }); //this accepts the value directly and shows it
+
+// Route::fallback(function () {
+//     return 'This path does not exist!';
+// }); // fallback redirect all wrong path and shows this message
