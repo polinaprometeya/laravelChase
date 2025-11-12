f0eaf62ad7c91db382abc6fed07fe32f61ae8b6a
Polina Sterobo <ppbs@intellifinder.dk>
2025-11-12 11:05:50 +0100

    lesson 11 - routing

--- ghost-laravel-app/routes/web.php
+++ ghost-laravel-app/routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
 });
+
+Route::get('/xxx', function () {
+    return 'Hello';
+})->name('route-name'); //when you name a route you give it alias that can always be used to identify it
+
+Route::get('/wrong', function () {
+    return redirect()->route('hello');
+}); //this helps to redirect different path somewhere
+
+Route::get('greet/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
+}); //this accepts the value directly and shows it
+
+Route::fallback(function () {
+    return 'This path does not exist!';
+}); // fallback redirect all wrong path and shows this message
