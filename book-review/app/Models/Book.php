d6389fa47a9de2528919de0dfc782a01b0e7cd41
Polina Sterobo <ppbs@intellifinder.dk>
2025-11-17 12:45:16 +0100

    Lesson 40 --local query scopes

--- book-review/app/Models/Book.php
+++ book-review/app/Models/Book.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use iLLuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
@@ -12,5 +13,10 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $titleSearch): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $titleSearch . '%');
     }
 }
