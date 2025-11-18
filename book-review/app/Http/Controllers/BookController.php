99eb86a3bf5d7608548656681215923ccff7b23c
Polina Sterobo <ppbs@intellifinder.dk>
2025-11-18 09:44:25 +0100

    Fixing route issue

--- book-review/app/Http/Controllers/BookController.php
+++ book-review/app/Http/Controllers/BookController.php
@@ -3,15 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $title = $request->input('title');
+        $books = Book::when(
+            $title,
+            fn ($query, $title) => $query->title($title)
+        )->get();
+
+        //  $books = Book::when($title, function ($query, $title) {
+        //  return  $query->title($title);
+        // })->get();
+        //this runs only when title is not null, it is a conditional statement helped by When
+
     }
 
     /**
--- book-review/routes/web.php
+++ book-review/routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('books', BookController::class);
+Route::resource('books', BookController::class);
