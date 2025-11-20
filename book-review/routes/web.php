260d06ce95fae7b11374efc8bea1a2af2086750d
Polina Sterobo <ppbs@intellifinder.dk>
2025-11-20 10:50:49 +0100

    Lesson 56 - Rate Limiting

--- book-review/routes/web.php
+++ book-review/routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Database\Eloquent\Collection;
 
 Route::get('/', function () {
     return redirect()->route('books.index');
@@ -12,9 +11,20 @@ Route::get('/', function () {
 Route::resource('books', BookController::class)
 ->only(['index','show']);
 
+
 Route::resource('books.reviews', ReviewController::class)
 ->scoped(['review' => 'book'])
 ->only(['create', 'store']);
+
+Route::post('books/{book}/reviews', [ReviewController::class, 'store'])
+->middleware('throttle:reviews')
+->name('books.reviews.store');
+
+//adding middleware  you need to specify that middleware applies only to store, or it will limit create too
+// Route::resource('books.reviews', ReviewController::class)
+// ->scoped(['review' => 'book'])
+// ->only(['create', 'store'])
+// ->middlewareFor(['store'], ['throttle:reviews']);
 
 Route::fallback(function () {
     return 'route does not exist';
