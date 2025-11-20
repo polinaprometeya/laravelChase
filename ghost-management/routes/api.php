3368e59da1564b6d8d7ffb7c3e6715636c851bb2
Polina Sterobo <ppbs@intellifinder.dk>
2025-11-20 15:05:41 +0100

    Lesson 60 -- scaffolding - making rest api app project

---
+++ ghost-management/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\Api\AttendeeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\EventController;
+
+//Route::middleware('auth:sanctum')->get('/user', function(Request $request){return $request->user();}); <--- laravel 11 , this is no more
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('events', EventController::class);
+
+Route::apiResource('event.attendees', AttendeeController::class)->scoped(['attendee' => 'event']);
+//-->attendee is always part of an event, if you use route model binding it will look for attendee in a parent event.
+//In that case they both parameter are required and need to be present or the query fails.  GET|HEAD        api/event/{event}/attendees/{attendee}
+
+//
