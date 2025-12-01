55c325d72d0fcf0b3b8824fd3af3cf1a649465a7
Polina Sterobo <ppbs@intellifinder.dk>
2025-12-01 15:19:38 +0100

    Lesson 69 -- loading attendee relations

--- ghost-management/app/Http/Controllers/Api/AttendeeController.php
+++ ghost-management/app/Http/Controllers/Api/AttendeeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AttendeeRequest;
 use App\Http\Resources\AttendeeResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Attendee;
 use Illuminate\Http\Request;
 use App\Models\Event;
@@ -12,15 +13,21 @@ use App\Models\User as UserModel;
 
 class AttendeeController extends Controller
 {
+    use CanLoadRelationships;
+
+    private array $relations = ['user'];
     /**
      * Display a listing of the resource.
      */
     public function index(Event $event)
     {
-        $data = $event->attendees()->latest();
-        $attendees = AttendeeResource::collection(
-            $data->paginate()
+        $attendees = $this->loadRelationships(
+            $event->attendees()->lates()
         );
+        // $data = $event->attendees()->latest();
+        // $attendees = AttendeeResource::collection(
+        //     $data->paginate()
+        // );
         return $attendees;
     }
 
@@ -29,7 +36,14 @@ class AttendeeController extends Controller
      */
     public function store(Request $request, Event $event)
     {
-        $data = $event->attendees()->create(['user_id' => 1]);
+        // $data = $event->attendees()->create(['user_id' => 1]);
+        // $attendee = new AttendeeResource($data);
+
+        $data = $this->loadRelationships(
+            $event->attendees()->create([
+                'user_id' => 1
+            ])
+        );
         $attendee = new AttendeeResource($data);
         return $attendee;
     }
@@ -39,7 +53,9 @@ class AttendeeController extends Controller
      */
     public function show(Event $event, Attendee $attendee)
     {
-        return new AttendeeResource($attendee);
+        // return new AttendeeResource($attendee);
+        $attendee = new AttendeeResource($this->loadRelationships($attendee));
+        return $attendee;
     }
 
     /**
--- ghost-management/app/Http/Traits/CanLoadRelationships.php
+++ ghost-management/app/Http/Traits/CanLoadRelationships.php
@@ -2,17 +2,18 @@
 
 namespace App\Http\Traits;
 
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 trait CanLoadRelationships
 {
     //to remind ? before a parameter make it optional
     public function loadRelationships(
-        Model | EloquentBuilder | QueryBuilder $for,
+        Model | EloquentBuilder | QueryBuilder | HasMany $for,
         ?array $relations = null
-    ): Model | EloquentBuilder | QueryBuilder {
+    ): Model | EloquentBuilder | QueryBuilder | HasMany {
         //if left argument is null, it will go on to the next one to the right, if that one is null it will take the last
         $relations = $relations ?? $this->relations ?? [];
 
