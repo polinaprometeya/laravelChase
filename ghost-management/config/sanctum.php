eec375878461020148c8affd4fa8afb5e50f076e
Polina Sterobo <ppbs@intellifinder.dk>
2025-12-04 10:37:43 +0100

    Lesson 73 -- Logout functionality

--- ghost-management/config/sanctum.php
+++ ghost-management/config/sanctum.php
@@ -47,7 +47,7 @@ return [
     |
     */
 
-    'expiration' => null,
+    'expiration' => 60 * 24, //login token expires after 24 hours
 
     /*
     |--------------------------------------------------------------------------
--- ghost-management/routes/api.php
+++ ghost-management/routes/api.php
@@ -14,6 +14,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])
+->middleware('auth:sanctum');
 
 Route::apiResource('events', EventController::class);
 
